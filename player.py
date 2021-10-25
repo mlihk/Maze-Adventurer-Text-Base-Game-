8f3732419e0a8342aa400261b7e9be966a1f070f
Atif Khan <khana75@cardiff.ac.uk>
2021-10-25 15:56:15 +0100

    CHANGES TO MATCH STORY

--- player.py
+++ player.py
@@ -2,5 +2,5 @@ from items import *
 from map import rooms
 player = {"name": "", "alive": True, "health": 100, "gold": 0}
 playerInventory = []
-# Start game at the beginning
+# Start game at the beginning of story
 current_room = rooms["Beginning"]
