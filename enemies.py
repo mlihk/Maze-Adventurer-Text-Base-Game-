e4311c16e3a95c1a772f6577382f8bdf77530a04
Dylan <PriceD20@cardiff.ac.uk>
2021-10-25 15:38:17 +0100

    New file enemies.py

---
+++ enemies.py
@@ -0,0 +1,24 @@
+#from items import *
+
+enemy_goblin = {
+    "name": "Goblin",
+
+    "death": "As the goblin falls back you here a mechanised cacophony of sound radiating from behind you."
+
+    }
+
+
+}
+
+enemy_mecha = {
+    "name": "Mecha Kirill",
+
+    "death": 'As little pops are heard of transistors and electronics breaking inside the looming structure, the hulking mass of metal and destruction falls back against the rear wall, conveniently catching the exit button, which now lights green. “Yoouu…ffaaaiillllll”, all signs of what could be considered life, are no-longer considered. '
+
+    }
+}
+
+rooms = {
+    "Goblin": enemy_goblin,
+    "Mecha": enemy_mecha
+}
