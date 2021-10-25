4ff77e1f460b43a802db32f4a052e3019503433c
Dylan <PriceD20@cardiff.ac.uk>
2021-10-25 16:01:08 +0100

    Added enemies.py

--- enemies.py
+++ enemies.py
@@ -5,9 +5,6 @@ enemy_goblin = {
 
     "death": "As the goblin falls back you here a mechanised cacophony of sound radiating from behind you."
 
-    }
-
-
 }
 
 enemy_mecha = {
@@ -15,7 +12,6 @@ enemy_mecha = {
 
     "death": 'As little pops are heard of transistors and electronics breaking inside the looming structure, the hulking mass of metal and destruction falls back against the rear wall, conveniently catching the exit button, which now lights green. “Yoouu…ffaaaiillllll”, all signs of what could be considered life, are no-longer considered. '
 
-    }
 }
 
 rooms = {
