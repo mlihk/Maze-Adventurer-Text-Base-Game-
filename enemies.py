8e7738f5d973ead67f794386eabba7db46c949c1
Dylan <PriceD20@cardiff.ac.uk>
2021-10-25 16:21:03 +0100

    updated enemies.py

--- enemies.py
+++ enemies.py
@@ -1,9 +1,35 @@
 #from items import *
 
-enemy_goblin = {
+enemy_goblin_on = {
     "name": "Goblin",
 
-    "death": "As the goblin falls back you here a mechanised cacophony of sound radiating from behind you."
+    "death": "As the goblin falls back you here a mechanised cacophony of sound radiating from behind you.",
+
+    "health": 20,
+
+    "base_damage": 5
+
+}
+
+enemy_goblin_oe = {
+    "name": "Goblin",
+
+    "death": "As the goblin falls back you here a mechanised cacophony of sound radiating from behind you.",
+
+    "health": 20,
+
+    "base_damage": 5
+
+}
+
+enemy_goblin_ow = {
+    "name": "Goblin",
+
+    "death": "As the goblin falls back you here a mechanised cacophony of sound radiating from behind you.",
+
+    "health": 20,
+
+    "base_damage": 5
 
 }
 
@@ -12,9 +38,14 @@ enemy_mecha = {
 
     "death": 'As little pops are heard of transistors and electronics breaking inside the looming structure, the hulking mass of metal and destruction falls back against the rear wall, conveniently catching the exit button, which now lights green. “Yoouu…ffaaaiillllll”, all signs of what could be considered life, are no-longer considered. '
 
+    "health": 100,
+
+    "base_damage": 30
 }
 
 rooms = {
-    "Goblin": enemy_goblin,
+    "Goblin ON": enemy_goblin_on,
+    "Goblin OE": enemy_goblin_oe,
+    "Goblin OW": enemy_goblin_ow,
     "Mecha": enemy_mecha
 }
