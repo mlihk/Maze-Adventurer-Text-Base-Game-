195e375e2f9f694a4d3d4c3bfbaeca391559ac6e
Atif Khan <khana75@cardiff.ac.uk>
2021-10-26 19:23:58 +0000

    ALL ENEMIES

---
+++ enemies.py
@@ -0,0 +1,91 @@
+enemy_goblin = {
+    "id": "goblin",
+    
+    "name": "average goblin",
+
+    "description": 
+    """As you enter the room, a cackle then cry comes from the corner,
+    bearing two knives, and pale green skin, the creature lunges at you""",
+
+    "defeat_messaage":
+    """As the goblin falls back you here a mechanised cacophony of
+    sound radiating from behind you."""
+
+    "health": 100,
+
+    "alive": True,
+
+    "damage": 10
+    }
+
+enemy_goblin2 = {
+    "id": "goblin2",
+    
+    "name": "above average goblin",
+
+    "description": 
+    """As you enter the room, a snigger and chuckle meet you from the
+    opposite corner, slightly shining eyes meet your gaze, their owner
+    lunges towards you.""",
+
+    "defeat_messaage":
+    """As the goblin falls back you here a mechanised cacophony of
+    sound radiating from behind you."""
+
+    "health": 100,
+
+    "alive": True,
+
+    "damage": 10
+    }
+
+enemy_goblin3 = {
+    "id": "goblin3",
+    
+    "name": "another goburin",
+
+    "description": 
+    """From the ceiling falls a creature with the grace of a rock,
+    he looks up to you and unsheathes its daggers.""",
+
+    "defeat_messaage":
+    """As the goblin falls back you here a mechanised cacophony of
+    sound radiating from behind you."""
+
+    "health": 100,
+
+    "alive": True,
+
+    "damage": 10
+    }
+
+enemy_kirill = {
+    "id": "kirill",
+    
+    "name": "MECHA KIRILL",
+
+    "description": 
+    """As you walk into the room, you can see a huge button displaying
+    exit lit up in red. Underneath, is a small compact construct with a small
+    head inside. The eyes begin to stare at you, as the construct slowly begins
+    to rise, lifting two cannons on each arm and standing over 9 feet tall.
+    “Computers are the future, the world will meet a new era of tyranny,
+    for I am MECHAKIRILL”*""",
+
+    "defeat_messaage":
+    """As little pops are heard of transistors and electronics breaking inside
+    the looming structure, the hulking mass of metal and destruction falls back
+    against the rear wall, conveniently catching the exit button, which now lights
+    green. “Yoouu…ffaaaiillllll”, all signs of what could be considered life,
+    are no-longer considered. """
+
+    "health": 100,
+
+    "alive": True,
+
+    "damage": 10
+    }
+
+
+
+
