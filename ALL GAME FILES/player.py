4587eccc0b3ed7e0018234312171e069f20262b9
Atif Khan <khana75@cardiff.ac.uk>
2021-10-27 12:54:22 +0000

    Delete player.py

--- ALL GAME FILES/player.py
+++
@@ -1,6 +0,0 @@
-from items import *
-from map import rooms
-player = {"name": "", "alive": True, "health": 100, "gold": 0}
-inventory = []
-# Start game at the beginning of story
-current_room = rooms["OS"]
