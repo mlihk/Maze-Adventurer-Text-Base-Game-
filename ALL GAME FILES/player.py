0e4283bb7ad261a5dcd39e1ba2578d72aeddccee
Atif Khan <khana75@cardiff.ac.uk>
2021-10-27 12:57:03 +0000

    player fully updated i believe

---
+++ ALL GAME FILES/player.py
@@ -0,0 +1,6 @@
+from items import *
+from map import rooms
+player = {"name": "", "alive": True, "health": 100, "gold": 0}
+inventory = []
+# Start game at the beginning of story
+current_room = rooms["OS"]
