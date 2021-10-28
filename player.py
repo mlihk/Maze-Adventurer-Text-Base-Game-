8aae3b3bf77ec9f0af4cf75e6425c90f45eef7bb
Kornel <krzyskowkg@cardiff.ac.uk>
2021-10-28 12:33:55 +0100

    Updated player health from 1 to 100

--- player.py
+++ player.py
@@ -3,7 +3,7 @@ from map import rooms
 
 inventory = []
 #inventory = [item_sword, item_hp_ring, item_potion]
-plrhealth = int(1)
+plrhealth = int(100)
 plrgold = int(0)
 # Start game at the beginning of story
 current_room = rooms["Beginning"]
